264a7f81c7249d73721dcd3f4a6032ec061206d6
Hausdorff Zhu <zorn.q.h@gmail.com>
2018-07-03 10:53:23 +0800

    format

--- script/main.js
+++ script/main.js
@@ -1,92 +1,108 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //init
-    var block_height=$("#left_button_panel").height()-18*$(".left_button").height();
+    var block_height = $("#left_button_panel").height() - 18 * $(".left_button").height();
     $("#blank_block").height(block_height);
     $("#accountT").show();
 
-    var wcp=$("#main").width()-27;
-    var wcps=wcp+"px";
-    $("#win_change").css({left:wcps});
+    var wcp = $("#main").width() - 27;
+    var wcps = wcp + "px";
+    $("#win_change").css({
+        left: wcps
+    });
     //end init
 
-    $(".left_button").click(function(){
+    $(".left_button").click(function () {
 
-        if($(this).attr("title")!="active"){
-            if($(this).attr("id")!="account"){
-                win_new($(this).attr("id"),0);
+        if ($(this).attr("title") != "active") {
+            if ($(this).attr("id") != "account") {
+                win_new($(this).attr("id"), 0);
             }
-            if($("#left_detail_panel").width()==0){
-                $("#main_window").animate({width:'81.8%'});
-                $("#left_detail_panel").animate({width:'15.7%'});
+            if ($("#left_detail_panel").width() == 0) {
+                $("#main_window").animate({
+                    width: '81.8%'
+                });
+                $("#left_detail_panel").animate({
+                    width: '15.7%'
+                });
             }
-            $(".left_button.active").attr("title","noactive");
-            $(".left_button.active").attr("class","left_button noactive");
-    
-            $(this).attr("class","left_button active");
-            $(this).attr("title","active");
+            $(".left_button.active").attr("title", "noactive");
+            $(".left_button.active").attr("class", "left_button noactive");
 
-        }else{
-            if($("#left_detail_panel").width()>0){
-                $("#left_detail_panel").animate({width:'0%'});
-                $("#main_window").animate({width:'97.5%'});
-                $(this).attr("title","noactive");
-                $(this).attr("class","left_button noactive");
-            }else{
-                $("#main_window").animate({width:'81.8%'});
-                $("#left_detail_panel").animate({width:'15.7%'});
-                $(this).attr("title","active");
-                $(this).attr("class","left_button active");
+            $(this).attr("class", "left_button active");
+            $(this).attr("title", "active");
+
+        } else {
+            if ($("#left_detail_panel").width() > 0) {
+                $("#left_detail_panel").animate({
+                    width: '0%'
+                });
+                $("#main_window").animate({
+                    width: '97.5%'
+                });
+                $(this).attr("title", "noactive");
+                $(this).attr("class", "left_button noactive");
+            } else {
+                $("#main_window").animate({
+                    width: '81.8%'
+                });
+                $("#left_detail_panel").animate({
+                    width: '15.7%'
+                });
+                $(this).attr("title", "active");
+                $(this).attr("class", "left_button active");
             }
         }
 
         $(".tool.active").hide();
-        $(".tool.active").attr("class","tool noactive");
-        var sel="#"+$(this).attr("id")+"T";
+        $(".tool.active").attr("class", "tool noactive");
+        var sel = "#" + $(this).attr("id") + "T";
         $(sel).show();
-        $(sel).attr("class","tool active");
+        $(sel).attr("class", "tool active");
 
     })
 
-    $("#exit_no").click(function(){
+    $("#exit_no").click(function () {
         $("#exit_panel").hide();
     })
 
-    $("#power").click(function(){
+    $("#power").click(function () {
         $("#exit_panel").show();
     })
 
-    $("#exit_yes").click(function(){
-        self.opener=null;
+    $("#exit_yes").click(function () {
+        self.opener = null;
         self.close();
     })
 
 
     //window change
-    $("#win_change").mouseover(function(){
-        if($("#right_window").css("display")=="none"){
-            $(this).attr("src","icon/add.png");
-        }else{
-            $(this).attr("src","icon/remove.png");
+    $("#win_change").mouseover(function () {
+        if ($("#right_window").css("display") == "none") {
+            $(this).attr("src", "icon/add.png");
+        } else {
+            $(this).attr("src", "icon/remove.png");
         }
     })
-    $("#win_change").mouseout(function(){
-        $(this).attr("src","icon/point.png");
+    $("#win_change").mouseout(function () {
+        $(this).attr("src", "icon/point.png");
     })
-    $("#win_change").click(function(){
-        if($("#right_window").css("display")=="none"){
-            $("#left_window").css({width:"50%"});
-            for(var i=4;i<=6;i++){
-                if($("#wt"+i).attr("id")){
-                    $("#right_window>.div_tab").append($("#wt"+i));
-                    $("#right_window>.div_panel").append($("#w"+i));
+    $("#win_change").click(function () {
+        if ($("#right_window").css("display") == "none") {
+            $("#left_window").css({
+                width: "50%"
+            });
+            for (var i = 4; i <= 6; i++) {
+                if ($("#wt" + i).attr("id")) {
+                    $("#right_window>.div_tab").append($("#wt" + i));
+                    $("#right_window>.div_panel").append($("#w" + i));
                 }
             }
-            if(!($("#leftP>.display").attr("class"))){
-                $("#"+$("#leftP").children()[0].id).attr("class","windows display");
+            if (!($("#leftP>.display").attr("class"))) {
+                $("#" + $("#leftP").children()[0].id).attr("class", "windows display");
             }
             $("#right_window").show();
-        }else{
-            $("#leftP>.display").attr("class","windows");
+        } else {
+            $("#leftP>.display").attr("class", "windows");
             //if($("#right_tab>.activeT").attr("id")){
             //   $("#left_window>.div_panel>.display").attr("class","windows");
             //}
@@ -94,71 +110,74 @@ $(document).ready(function(){
             $("#left_window>.div_tab").append($("#right_window>.div_tab").children());
             $("#left_window>.div_panel").append($("#right_window>.div_panel").children());
             $("#right_window").hide();
-            $("#left_window").css({width:"100%"});
+            $("#left_window").css({
+                width: "100%"
+            });
         }
     })
 
     $("#infoP").on("mousewheel DOMMouseScroll", function (e) {
-        var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || 
-                    (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
-                            var top=parseInt($(".divinfo").css("top"));
-        top+=delta*60;
-        (top<=0)||(top=0);
-        (top>-($("#infoP").children().length*100-500))||(top=-($("#infoP").children().length*100-500));
-        $(".divinfo").css({top:top+"px"});
+        var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||
+            (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
+        var top = parseInt($(".divinfo").css("top"));
+        top += delta * 60;
+        (top <= 0) || (top = 0);
+        (top > -($("#infoP").children().length * 100 - 500)) || (top = -($("#infoP").children().length * 100 - 500));
+        $(".divinfo").css({
+            top: top + "px"
+        });
     });
 
-    $(".divinfo").click(function(){
-        $(".divinfo.active").attr("class","divinfo");
-        $(this).attr("class","divinfo active");
+    $(".divinfo").click(function () {
+        $(".divinfo.active").attr("class", "divinfo");
+        $(this).attr("class", "divinfo active");
     })
 })
 
-function tab_click(id){
-    if($("#wt"+id).attr("id")){
-        $(".tab.activeT").attr("class","tab noactiveT");
-        $("#wt"+id).attr("class","tab activeT");
-        var app_type=$("#wt"+id+">.tab_img").attr("src").split("/")[1].split("A")[0];
-        app_type="#"+app_type+"T";
+function tab_click(id) {
+    if ($("#wt" + id).attr("id")) {
+        $(".tab.activeT").attr("class", "tab noactiveT");
+        $("#wt" + id).attr("class", "tab activeT");
+        var app_type = $("#wt" + id + ">.tab_img").attr("src").split("/")[1].split("A")[0];
+        app_type = "#" + app_type + "T";
         $(".tool.active").hide();
-        $(".tool.active").attr("class","tool noactive");
+        $(".tool.active").attr("class", "tool noactive");
         $(app_type).show();
-        $(app_type).attr("class","tool active");
-        var par=$("#wt"+id).parent();
-        var side="#"+$(par).attr("title")+"P";
-        if($(side+">.display").attr("id")){
-            $(side+">.display").attr("class","windows");
+        $(app_type).attr("class", "tool active");
+        var par = $("#wt" + id).parent();
+        var side = "#" + $(par).attr("title") + "P";
+        if ($(side + ">.display").attr("id")) {
+            $(side + ">.display").attr("class", "windows");
         }
-        $("#w"+id).attr("class","windows display");
+        $("#w" + id).attr("class", "windows display");
     }
 }
 
 //drag window
-function allowDrop(ev)
-{
+function allowDrop(ev) {
     ev.preventDefault();
 }
-function tab_drag(ev)
-{
-    ev.dataTransfer.setData("Text",ev.target.id);
-}
-function tab_drop(ev)
-{
-    if($("#"+ev.target.id).children().length<3){
-        ev.preventDefault();
-        var data=ev.dataTransfer.getData("Text");
-        ev.target.appendChild(document.getElementById(data));
-        if($("#"+$(ev.target).attr("title")+"P>.display").attr("id")){
-            $("#"+$(ev.target).attr("title")+"P>.display").attr("class","windows");
-        }
-        $("#"+$(ev.target).attr("title")+"P").append($("#w"+data[2]));
-        
 
-        if((!($("#leftP>.display").attr("id")))&&($("#leftP").children())){
-            $($("#leftP").children()[0]).attr("class","windows display");
+function tab_drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function tab_drop(ev) {
+    if ($("#" + ev.target.id).children().length < 3) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("Text");
+        ev.target.appendChild(document.getElementById(data));
+        if ($("#" + $(ev.target).attr("title") + "P>.display").attr("id")) {
+            $("#" + $(ev.target).attr("title") + "P>.display").attr("class", "windows");
         }
-        if((!($("#rightP>.display").attr("id")))&&($("#rightP").children())){
-            $($("#rightP").children()[0]).attr("class","windows display");
+        $("#" + $(ev.target).attr("title") + "P").append($("#w" + data[2]));
+
+
+        if ((!($("#leftP>.display").attr("id"))) && ($("#leftP").children())) {
+            $($("#leftP").children()[0]).attr("class", "windows display");
+        }
+        if ((!($("#rightP>.display").attr("id"))) && ($("#rightP").children())) {
+            $($("#rightP").children()[0]).attr("class", "windows display");
         }
     }
 }
--- script/winManagement.js
+++ script/winManagement.js
@@ -1,65 +1,64 @@
-
-function win_init(){
+function win_init() {
 
 }
 
-function win_new(app_type,sel_paient){
+function win_new(app_type, sel_paient) {
     var win_id;
-    for(var i=1;i<=6;i++){
-        if(!($("#wt"+i).attr("id"))){
-            win_id=i;
+    for (var i = 1; i <= 6; i++) {
+        if (!($("#wt" + i).attr("id"))) {
+            win_id = i;
             break;
         }
     }
 
-    var id="wt"+win_id;
-    var new_w='<div class="windows display" id="w'+win_id+'">'+win_id+'</div>'
-    var new_tab='<div class="tab noactiveT" id="'+id+'" onclick="tab_click('+win_id+')" draggable="true" ondragstart="tab_drag(event)">';
-    new_tab+='<img class="tab_img" src="icon/'+app_type+'A.png" style="width:25px;height:25px;float:left;">';
-    new_tab+='<p class="tab_title" style="width:120px;height:100%;color:#2d2d2d;float:left;color:#fff" >test</p>';
-    new_tab+='<img class="tab_button" src="icon/down.png" style="width:25px;height:25px;float:left;">';
-    new_tab+='<img class="tab_button" id="tab_close" onclick="win_close('+win_id+')" src="icon/out.png" style="width:20px;height:20px;float:left;padding:4px;margin-top:3px;">';
-    new_tab+='</div>';
-    if(win_id){
-        if($("#right_window").css("display")=="none"){
-            if($("#left_window>.div_tab").children().length<6){
+    var id = "wt" + win_id;
+    var new_w = '<div class="windows display" id="w' + win_id + '">' + win_id + '</div>'
+    var new_tab = '<div class="tab noactiveT" id="' + id + '" onclick="tab_click(' + win_id + ')" draggable="true" ondragstart="tab_drag(event)">';
+    new_tab += '<img class="tab_img" src="icon/' + app_type + 'A.png" style="width:25px;height:25px;float:left;">';
+    new_tab += '<p class="tab_title" style="width:120px;height:100%;color:#2d2d2d;float:left;color:#fff" >test</p>';
+    new_tab += '<img class="tab_button" src="icon/down.png" style="width:25px;height:25px;float:left;">';
+    new_tab += '<img class="tab_button" id="tab_close" onclick="win_close(' + win_id + ')" src="icon/out.png" style="width:20px;height:20px;float:left;padding:4px;margin-top:3px;">';
+    new_tab += '</div>';
+    if (win_id) {
+        if ($("#right_window").css("display") == "none") {
+            if ($("#left_window>.div_tab").children().length < 6) {
                 $("#left_window>.div_tab").append(new_tab);
                 $("#leftP").append(new_w);
-                $(".windows.display").attr("class","windows");
-                $("#w"+win_id).attr("class","windows display");
+                $(".windows.display").attr("class", "windows");
+                $("#w" + win_id).attr("class", "windows display");
             }
-        }else{
-            if($("#left_window>.div_tab").children().length<3){
+        } else {
+            if ($("#left_window>.div_tab").children().length < 3) {
                 $("#left_window>.div_tab").append(new_tab);
                 $("#leftP").append(new_w);
-                $("#leftP>.display").attr("class","windows");
-                $("#w"+win_id).attr("class","windows display");
-            }else{
+                $("#leftP>.display").attr("class", "windows");
+                $("#w" + win_id).attr("class", "windows display");
+            } else {
                 $("#right_window>.div_tab").append(new_tab);
                 $("#rightP").append(new_w);
-                $("#rightP>.display").attr("class","windows bn");
-                $("#w"+win_id).attr("class","windows display");
+                $("#rightP>.display").attr("class", "windows bn");
+                $("#w" + win_id).attr("class", "windows display");
             }
         }
-        $(".tab.activeT").attr("class","tab noactiveT");
-        $("#"+id).attr("class","tab activeT");
+        $(".tab.activeT").attr("class", "tab noactiveT");
+        $("#" + id).attr("class", "tab activeT");
 
     }
 
 
 }
 
-function win_close(sel){
-    $("#wt"+sel).remove();
-    $("#w"+sel).remove();
-    if((!($(".activeT").attr("id")))&&($("#leftP").children())){
+function win_close(sel) {
+    $("#wt" + sel).remove();
+    $("#w" + sel).remove();
+    if ((!($(".activeT").attr("id"))) && ($("#leftP").children())) {
         tab_click($($("#leftP").children()[0]).attr("id")[1]);
     }
-    if((!($("#leftP>.display").attr("id")))&&($("#leftP").children())){
-        $($("#leftP").children()[0]).attr("class","windows display");
+    if ((!($("#leftP>.display").attr("id"))) && ($("#leftP").children())) {
+        $($("#leftP").children()[0]).attr("class", "windows display");
     }
-    if((!($("#rightP>.display").attr("id")))&&($("#rightP").children())){
-        $($("#rightP").children()[0]).attr("class","windows display");
+    if ((!($("#rightP>.display").attr("id"))) && ($("#rightP").children())) {
+        $($("#rightP").children()[0]).attr("class", "windows display");
     }
-    
+
 }
